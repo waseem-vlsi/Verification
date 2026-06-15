9f9214eb143fc74ced8064fdf7c5c724b1be1409
waseem-vlsi <2025pvl0112@iitjammu.ac.in>
2026-06-15 12:52:42 +0530

    Create transaction_class.sv

---
+++ D_ff/transaction_class.sv
@@ -0,0 +1,22 @@
+class trasaction;
+
+rand logic d_in;
+     logic d_out;
+
+  function transaction copy();
+
+    copy = new();
+    copy.d_in = this.d_in;
+    copy.d_out = this.d_out;
+
+  endfunction
+
+  function void display(input string tag);
+
+    $display("[%0s]: d_in = %0d and d_out : %0d", tag,d_in,d_out);
+
+  endfunction
+
+
+
+endclass 
