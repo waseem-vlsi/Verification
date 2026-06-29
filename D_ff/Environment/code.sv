9f48391c2efae356432a2f0cd8b5142f290a942e
waseem-vlsi <2025pvl0112@iitjammu.ac.in>
2026-06-29 12:49:45 +0530

    Update code.sv

--- D_ff/Environment/code.sv
+++ D_ff/Environment/code.sv
@@ -29,7 +29,30 @@ class environment;
     gen.sconext = next;
     sco.sconext = next;
 
-
   endfunction
+
+  task pre_test();
+    drv.reset();
+  endtask
+
+  task test();
+    fork 
+      gen.run();
+      drv.run();
+      mon.run();
+      sco.run();  
+    join
+  endtask
+
+  task post_test();
+    wait(gen.done.triggered);
+  endtask
+
+  task run();
+    pre_test();
+    test();
+    post_test();
+  endtask
+  
 
 endclass
