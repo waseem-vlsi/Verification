7e2b023af28080ad7b26f3a98c0f1dba8d0029e9
waseem-vlsi <2025pvl0112@iitjammu.ac.in>
2026-06-30 11:37:00 +0530

    Update code.sv

--- Synchronous_FIFO/transaction_class/code.sv
+++ Synchronous_FIFO/transaction_class/code.sv
@@ -1,18 +1,24 @@
 class transaction;
 
   rand logic [4:0] din;
-  logic wr,rd;
+  rand logic wr,rd;
   logic [4:0] dout;
   logic full,empty;
 
 
   function transaction copy();
     copy = new();
-    this.din = din;
+    copy.din = this.din ;
+    copy.wr = this.wr;
+    copy.rd = this.rd;
+    copy.dout = this.dout;
+    copy.full = this.full;
+    copy.empty = this.empty;
   endfunction
 
-  function void display(input string);
-    $display("[%0s] : din = %0d", string,din);
+  function void display(input string tag);
+    $display("[%0s] : din=%0d wr=%0b rd=%0b dout=%0d full=%0b empty=%0b",
+          tag,din,wr,rd,dout,full,empty);
 
   endfunction
 endclass
