6fe72a06faf13aaef28d74ddafd24e7aac99cdfc
waseem-vlsi <2025pvl0112@iitjammu.ac.in>
2026-06-30 11:27:28 +0530

    Update code.sv

--- Synchronous_FIFO/interface/code.sv
+++ Synchronous_FIFO/interface/code.sv
@@ -1,12 +1,48 @@
-interface sync_FIFO;
-  input logic clock;
-  input logic reset;
-  input logic [4:0]din;
-  input logic wr;
-  input logic rd;
-  output logic [4:0]dout;
-  output logic full;
-  output logic empty;
+interface sync_FIFO_if;
 
+   logic clock;
+   logic reset;
+   logic [4:0] din;
+   logic wr;
+   logic rd;
+   logic [4:0] dout;
+   logic full;
+   logic empty;
+
+   modport drv(
+      input clock,
+
+      output reset,
+      output din,
+      output wr,
+      output rd,
+
+  
+      input full,
+      input empty
+   );
+
+   modport mon(
+      input clock,
+      input reset,
+      input din,
+      input dout,
+      input wr,
+      input rd,
+      input full,
+      input empty
+   );
+
+   modport dut(
+      input clock,
+      input reset,
+      input din,
+      input wr,
+      input rd,
+
+      output dout,
+      output full,
+      output empty
+   );
 
 endinterface
