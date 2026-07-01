dff9e0b928b36631451cc7140d547bf9e56ddf45
waseem-vlsi <2025pvl0112@iitjammu.ac.in>
2026-07-01 09:49:55 +0530

    Update code.sv

--- Synchronous_FIFO/gen_drv_mon_sco/code.sv
+++ Synchronous_FIFO/gen_drv_mon_sco/code.sv
@@ -24,8 +24,49 @@ class generator;
       $display("[Gen] : din = %0d",tr.din)
     end 
   endtask
-  
-
-
-
 endclass
+
+
+
+
+class driver ;
+
+  generator gr;
+  mailbox #(transaction) dgmbx;
+  virtual dif_if vif;
+
+  function new (mailbox #(transaction) dgmbx);
+    this.dgmbx = dgmbx;
+    gr = new();
+  endfunction
+
+  task reset();
+    vif.reset <= 1'b1;
+    repeat(5)@posedge vif.clock;
+    vif.reset <= 1'b0;
+    @posedge vif.clock;
+  endtask
+
+  task run();
+    forever begin 
+      dgmbx.get(tr);
+      if(vif.wr) begin 
+        vif.wr <= 1'b1;
+        vif.rd <= 1'b0;
+        vif.din <= tr.din;
+      end 
+      else if(vif.rd) begin 
+        vif.wr <= 1'b0;
+        vif.rd <= 1'b1;
+      end 
+      
+
+    end 
+    
+
+  endtask
+
+
+
+
+endclass 
