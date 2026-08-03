3a01009d83a269e6623cdc0355a5b98cacac428d
waseem-vlsi <2025pvl0112@iitjammu.ac.in>
2026-08-03 13:10:33 +0530

    Update code.sv

--- D_FF_1/Generator/code.sv
+++ D_FF_1/Generator/code.sv
@@ -1,6 +1,22 @@
 class generator;
-  dff_if vif;
-  
+  dff_transaction tr;
+  mailbox #(dff_transaction) mbxgd;
+  mailbox #(dff_transaction) mbxgs;
+  integer count;
+  function new(mailbox #(dff_transaction) mbxgd, mailbox #(dff_transaction) mbxgs);
+    tr = new();
+    this.mbxgd = mbxgd;
+    this.mbxgs = mbxgs;
+  endfunction
 
-
+  task run();
+    repeat(count) begin 
+      assert(tr.randomize()) else begin 
+        $display("Randomization failed!!!");
+      end 
+      tr.display("Gen");
+      mbxgd.put(tr.copy());
+      mbxgs.put(tr.copy());
+    end 
+  endtask
 endclass
