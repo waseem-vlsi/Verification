447fd6ba95342f3600c930ecfa219f0f2d5468c1
waseem-vlsi <2025pvl0112@iitjammu.ac.in>
2026-08-05 17:03:38 +0530

    Update code.sv

--- sync_fifo_2/scoreboard/code.sv
+++ sync_fifo_2/scoreboard/code.sv
@@ -1,5 +1,47 @@
 class scoreboard;
+  sync_fifo_transaction tr;
+  sync_fifo_transaction trref;
+
+  logic [7:0] ref_fifo[$];
+  logic [7:0] expected_data;
+  
   mailbox #(sync_fifo_transaction) mbxms;
   mailbox #(sync_fifo_transaction) mbxgs; 
+
+  function new (mailbox #(sync_fifo_transaction) mbxms, mailbox #(sync_fifo_transaction) mbxgs);
+    this.mbxms = mbxms;
+    this.mbxgs = mbxgs;
+  endfunction
+
+
+  task run();
+    forever begin 
+    mbxms.get(tr);
+    mbxgs.get(trref);
+
+      if(trref.wrenable && !trref.full) begin 
+        ref_fifo.push_back(trref.din);
+        $display("[SCO] : Write: Data : %0d pushed into queue", trref.din);
+      end 
+      
+      
+      if(trref.rdenable && !trref.empty) begin 
+        if(ref_fifo.size() != 0) begin 
+          expected_data = ref_fifo.pop_front();
+          if(expected_data == tr.dout) begin 
+            $display("Data Matched!!!");
+          end 
+          else begin 
+            $display("Data Mismatched!!!");
+          end 
+        end 
+                else begin
+
+          $display("[SCO] ERROR : Reference FIFO Empty");
+
+        end
+      end 
+    end
+  endtask 
 
 endclass
